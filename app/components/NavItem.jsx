5c613d91e886c98305f51fcfa37d94834219d7fa
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-03-08 18:32:48 +0400

    chore: add eslint as a dependency to improve code quality

--- app/components/NavItem.jsx
+++ app/components/NavItem.jsx
@@ -10,15 +10,17 @@ const NavItem = ({ item }) => {
     >
       <a href={item.href}>{item.label}</a>
       <img src="/chevronDown.svg" alt="chevron" />
-      {isOpen && (
-        <ul>
-          {item.subLinks.map((sub, index) => (
-            <li key={index}>
-              <Link href={sub.href}>{sub.label}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      <div>
+        {isOpen && (
+          <ul>
+            {item.subLinks.map((sub, index) => (
+              <li key={index}>
+                <Link href={sub.href}>{sub.label}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </li>
   );
 };
