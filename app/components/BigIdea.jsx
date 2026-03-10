01a7a5041347edb4400650ec7086a64852fb5c68
Javid Rashidov <rashidovjavid1@gmail.com>
2026-03-10 19:05:37 +0400

    feat: enhance BigIdea component with new layout and video; add earth.mp4 asset

--- app/components/BigIdea.jsx
+++ app/components/BigIdea.jsx
@@ -7,13 +7,26 @@ const BigIdea = () => {
     <section className="BigIdea">
       <div className="container">
         <div className="bigIdeaMain">
-          <video
-            src="/assets/earth.mp4"
-            autoPlay
-            loop
-            playsInline
-            preload="auto"
-          />
+          <div className="bigIdeaLeft">
+            <div className="bigIdeaLeftTop">
+              <h1 className="heroText1">What is the</h1>
+              <h1>Big IDEA?</h1>
+              <p>
+                Our mission is to create real and virtual communities of youth
+                at the regional and international levels, to develop that
+                community through open dialogues that provide information and
+                make practical calls on environmental protection, and to ensure
+                that each member of this community grows as a leader in order to
+                carry out environmental education work on the ground.
+              </p>
+            </div>
+            p
+          </div>
+          <div className="bigIdeaRight">
+            <div className="bigIdeaGif">
+              <video src="/earth.mp4" autoPlay loop playsInline muted></video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
