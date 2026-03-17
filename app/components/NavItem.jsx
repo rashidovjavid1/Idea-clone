7c2be2a130fa82838f1823638e03b98f34a7ecdf
Javid Rashidov <rashidovjavid1@gmail.com>
2026-03-17 20:58:30 +0400

    feat: Add ChevronDown icon and update NavItem and Header styles for improved dark mode support

--- app/components/NavItem.jsx
+++ app/components/NavItem.jsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
 import React, { useState } from "react";
+import ChevronDown from "@/public/chevronDown.svg";
+import { usePathname } from "next/navigation";
+import { darkPages } from "../constants/darkPages";
 
 const NavItem = ({ item }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathName = usePathname();
+  const isDark = darkPages.includes(pathName);
   return (
     <li
-      className={isOpen ? "open" : ""}
+      className={`${isOpen ? "open" : ""} ${isDark ? "dark--page" : ""}`}
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
     >
       <a href={item.href}>{item.label}</a>
-      <img src="/chevronDown.svg" alt="chevron" />
+      <ChevronDown />
       <div className="block"></div>
       <ul className={isOpen ? "open" : ""}>
         {item.subLinks.map((sub, index) => (
