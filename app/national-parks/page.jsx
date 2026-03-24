fa66e01f328a1a30599def262a29ae8078bb2647
Javid Rashidov <rashidovjavid1@gmail.com>
2026-03-24 20:30:49 +0400

    feat: Add resources styling for national parks page; enhance layout and design

--- app/national-parks/page.jsx
+++ app/national-parks/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { nationalParks } from "../constants/nationalParks";
+import "./resources.scss";
 
 const page = () => {
   return (
@@ -18,7 +19,7 @@ const page = () => {
         <div className="resourcesMain">
           <h1>The Natural Wealth of Azerbaijan</h1>
           <div className="resourcesTop">
-            <div className="nationalParksBannerLeft">
+            <div className="resourcesLeft">
               <div className="pageBanner">
                 <img
                   src="/nationalParksBanner.png"
@@ -26,23 +27,23 @@ const page = () => {
                 />
               </div>
             </div>
-            <div className="nationalParksBannerRight">
-              <div className="nationalParksTitle">
+            <div className="resourcesRight">
+              <div className="resourcesRightContent">
                 <span>
                   Azerbaijan is home to one of the most diverse ecosystems in
                   the Caucasus region.
                 </span>
-              </div>
-              <div className="nationalParksTitleDescription">
-                <p>
-                  From majestic mountain ranges and fertile valleys to lush
-                  forests and the Caspian Sea, the country shelters thousands of
-                  species of flora and fauna.
-                </p>
-                <p>
-                  This rich biodiversity makes Azerbaijan a true natural
-                  treasure and a vital habitat for many endangered species.
-                </p>
+                <div className="resourcesDescription">
+                  <p>
+                    From majestic mountain ranges and fertile valleys to lush
+                    forests and the Caspian Sea, the country shelters thousands
+                    of species of flora and fauna.
+                  </p>
+                  <p>
+                    This rich biodiversity makes Azerbaijan a true natural
+                    treasure and a vital habitat for many endangered species.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -57,24 +58,25 @@ const page = () => {
           <div className="resourcesBottomMain">
             <div className="container">
               <div className="resourceCards">
-                <div className="container">
-                  {nationalParks.map((item, index) => (
-                    <div key={index} className="resourceCard">
-                      <div className="cardBanner">
-                        <img src={item.cardBanner} alt={item.cardFirstTitle} />
+                {nationalParks.map((item, index) => (
+                  <div key={index} className="resourceCard">
+                    <div className="cardBanner">
+                      <img src={item.cardBanner} alt={item.cardFirstTitle} />
+                    </div>
+                    <div className="right">
+                      <div className="firstText">
+                        <span>{item.cardFirstTitle}</span>
                       </div>
-                      <div className="right">
-                        <div className="firstText">
-                          <span>{item.cardFirstTitle}</span>
-                        </div>
-                        <div className="cardTitle">
-                          <h3>{item.cardTitle}</h3>
+                      <div className="cardTitle">
+                        <h3>{item.cardTitle}</h3>
+
+                        <div className="cardDescription">
                           <p>{item.cardDescription}</p>
                         </div>
                       </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
