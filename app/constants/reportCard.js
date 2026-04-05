f545d55ef3e4ccf37abeb20c64345f31163a4463
Javid Rashidov <rashidovjavid1@gmail.com>
2026-04-06 02:49:50 +0400

    Add reports page with card layout and styling; include report data and images

---
+++ app/constants/reportCard.js
@@ -0,0 +1,44 @@
+export const reportsCards = [
+  {
+    image: "report1.jpg",
+    label: "IDEA Activity Report 2012-2022",
+    description:
+      "The publication reflects the activities of IDEA Public Union during the years 2012-2022.",
+    link: "/report1",
+  },
+  {
+    image: "report2.jpg",
+    label: "IDEA Activity Report 2017-2019",
+    description:
+      "The publication reflects the activities of IDEA Public Union during the years 2012-2022.",
+    link: "/report1",
+  },
+  {
+    image: "report3.png",
+    label: "IDEA Activity Report 2012-2022",
+    description:
+      "The publication reflects the activities of IDEA Public Union during the years 2012-2022.",
+    link: "/report1",
+  },
+  {
+    image: "report3.png",
+    label: "IDEA Activity Report 2012-2022",
+    description:
+      "The publication reflects the activities of IDEA Public Union during the years 2012-2022.",
+    link: "/report1",
+  },
+  {
+    image: "report2.jpg",
+    label: "IDEA Activity Report 2012-2022",
+    description:
+      "The publication reflects the activities of IDEA Public Union during the years 2012-2022.",
+    link: "/report1",
+  },
+  {
+    image: "report1.jpg",
+    label: "IDEA Activity Report 2012-2022",
+    description:
+      "The publication reflects the activities of IDEA Public Union during the years 2012-2022.",
+    link: "/report1",
+  },
+];
--- app/reports/page.jsx
+++ app/reports/page.jsx
@@ -1,4 +1,8 @@
 import "./reports.scss";
+import { reportsCards } from "../constants/reportCard";
+import MoreButton from "@/public/moreButton.svg";
+import Link from "next/link";
+import Footer from "../components/Footer";
 
 const page = () => {
   return (
@@ -14,7 +18,31 @@ const page = () => {
             </li>
           </ul>
         </div>
+        <div className="reportsMain">
+          <div className="pageTitle">
+            <span>FACTS</span>
+            <h1>REPORTS</h1>
+          </div>
+          <div className="reportsCards">
+            {reportsCards.map((item, index) => (
+              <div key={index} className="reportsCardItem">
+                <div className="reportsImage">
+                  <img src={item.image} alt={item.label} />
+                </div>
+                <div className="title">
+                  <h3>{item.label}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <Link href={item.link}>
+                  <span>View report </span>
+                  <MoreButton />
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
