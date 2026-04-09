3a71c3e94b6de65b56a875ceb1aa5f7c26e80f87
Javid Rashidov <rashidovjavid1@gmail.com>
2026-04-09 22:28:38 +0400

    Add project categories and implement filtering functionality on projects page

--- app/constants/projectCard.js
+++ app/constants/projectCard.js
@@ -3,32 +3,36 @@ export const projectCard = [
     image: "/project1.png",
     label: "Reuse for the Environment youth summer camp",
     date: "20 July 2013",
+    category: "Education and Training",
   },
   {
     image: "/project2.png",
-    label:
-      "Summer school on the topic 'Protection of the environment for the sake of present and future generations'",
+    label: "Summer school on the topic '...'",
     date: "30 August 2012",
+    category: "Education and Training",
   },
   {
     image: "/project3.png",
     label: "IDEA Green conversation club",
     date: "20 July 2013",
+    category: "Public and Engagement",
   },
   {
     image: "/project4.png",
     label: "Environmental Textbook Project",
     date: "20 July 2013",
+    category: "Innovation Dialogue",
   },
   {
     image: "/project5.png",
     label: "IDEA Lectures has been Launched",
     date: "20 July 2013",
+    category: "Advocacy and Public Awareness",
   },
   {
     image: "/project6.png",
-    label:
-      "IDEA Campaign Announces Essay Competition Send Your Message to Rio +20",
+    label: "IDEA Campaign Announces Essay Competition...",
     date: "20 July 2013",
+    category: "Wildlife Conservation and Animal Protection",
   },
 ];
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "./projects.scss";
 import { projectsCategories } from "../constants/projectsCategories";
 import Link from "next/link";
@@ -8,6 +8,11 @@ import Calendar from "@/public/calendar.svg";
 import Footer from "../components/Footer";
 
 const page = () => {
+  const [activeCategory, setActiveCategory] = useState("All");
+  const filteredCards =
+    activeCategory === "All"
+      ? projectCard
+      : projectCard.filter((card) => card.category === activeCategory);
   return (
     <div className="projectsInner">
       <div className="container">
@@ -23,11 +28,17 @@ const page = () => {
         <div className="projectsMain">
           <h1>Explore Our Projects</h1>
           <div className="categories">
-            <div className="category">
+            <div
+              onClick={() => setActiveCategory("All")}
+              className={`category ${activeCategory === "All" ? "active" : ""}`}
+            >
               <span>All</span>
             </div>
             {projectsCategories.map((item, index) => (
-              <button className="category">
+              <button
+                className={`category ${activeCategory === item.label ? "active" : ""}`}
+                onClick={() => setActiveCategory(item.label)}
+              >
                 <div className="image">
                   <img src={item.icon} alt={item.label} />
                 </div>
@@ -35,8 +46,9 @@ const page = () => {
               </button>
             ))}
           </div>
+
           <div className="projectCards">
-            {projectCard.map((item, index) => (
+            {filteredCards.map((item, index) => (
               <div className="projectCard">
                 <div className="projectImage">
                   <img src={item.image} alt={item.label} />
